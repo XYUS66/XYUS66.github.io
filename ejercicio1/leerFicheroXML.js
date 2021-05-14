1b4f1280bab830c436a7aa264e5fda2ed1b51be8
Marcos Tejera <76426487+XYUS66@users.noreply.github.com>
2021-05-14 09:54:02 +0200

    Update leerFicheroXML.js

--- ejercicio1/leerFicheroXML.js
+++ ejercicio1/leerFicheroXML.js
@@ -1,8 +1,8 @@
 function gestionarFicheroXML(xmlDoc){
 alert (xmlDoc)
-	let libro = xmlDoc.querySelectorAll("libro")
-	for(let i=0; i<libro.length; i++)
-		capaVacia.innerHTML = capaVacia.innerHTML + "<p>" + libro[i].textContent + "</p>"
+	let libros = xmlDoc.querySelectorAll("libro")
+	for(let i=0; i<libros.length; i++)
+		capaVacia.innerHTML = capaVacia.innerHTML + "<p>" + libros[i].textContent + "</p>"
 	
 }
 loadDocA("libros.xml","xml")
