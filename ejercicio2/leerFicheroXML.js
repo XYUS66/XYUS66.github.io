29c45e2f609efbbaad550708a18d5f6530f13eee
Marcos Tejera <76426487+XYUS66@users.noreply.github.com>
2021-05-20 21:04:36 +0200

    Update leerFicheroXML.js

--- ejercicio2/leerFicheroXML.js
+++ ejercicio2/leerFicheroXML.js
@@ -4,11 +4,11 @@ alert (xmlDoc)
 	let capaVacia = document.querySelector("#ficheroXML")
   let dcha = xmlDoc.querySelectorAll("derecha")
 	let izda = xmlDoc.querySelectorAll("izquierda")
-  let fondo = xmlDoc.querySelectorAll("chat")
+  let fondo = xmlDoc.querySelector("chat")
   fondo.className="fondo"
 	for(let i=0; i<dcha.length&&izda.length; i++){
 		capaVacia.innerHTML = capaVacia.innerHTML + "<p class="derecha">" + dcha[i].textContent + "</p>"
-		capaVacia.innerHTML = capaVacia.innerHTML + "<pclass="izquierda">" + izda[i].textContent + "</p>"
+		capaVacia.innerHTML = capaVacia.innerHTML + "<p class="izquierda">" + izda[i].textContent + "</p>"
   }
 }
 loadDocA("chat.xml","xml")
