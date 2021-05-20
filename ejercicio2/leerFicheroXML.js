b381eea5f22a8c376fcf483915f8f6bd252fb5cf
Marcos Tejera <76426487+XYUS66@users.noreply.github.com>
2021-05-20 22:02:24 +0200

    Update leerFicheroXML.js

--- ejercicio2/leerFicheroXML.js
+++ ejercicio2/leerFicheroXML.js
@@ -1,12 +1,12 @@
 
 function gestionarFicheroXML(xmlDoc){
 alert (xmlDoc)
-	let capaVacia = document.querySelector("#ficheroXML")
-  let dcha = xmlDoc.querySelectorAll("derecha")
-	let izda = xmlDoc.querySelectorAll("izquierda")
+let capaVacia = document.querySelector("#ficheroXML")
+let dcha = xmlDoc.querySelectorAll("derecha")
+let izda = xmlDoc.querySelectorAll("izquierda")
   let fondo = xmlDoc.querySelector("chat")
+  capaVacia.innerHTML = capaVacia.innerHTML + "<body class='fondo'>" + "</body>"
 	for(let i=0; i<dcha.length&&izda.length; i++){
-		capaVacia.innerHTML = capaVacia.innerHTML + "<div class='fondo'>" + "</div>"
 		capaVacia.innerHTML = capaVacia.innerHTML + "<p class='derecha'>" + dcha[i].textContent + "</p>"
 		capaVacia.innerHTML = capaVacia.innerHTML + "<p class='izquierda'>" + izda[i].textContent + "</p>"
   }
