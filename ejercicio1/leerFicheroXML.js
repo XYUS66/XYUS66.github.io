bcfaf9284c8bd428e81702f848c17cc62e35b67a
Marcos Tejera <76426487+XYUS66@users.noreply.github.com>
2021-05-20 10:27:56 +0200

    Update leerFicheroXML.js

--- ejercicio1/leerFicheroXML.js
+++ ejercicio1/leerFicheroXML.js
@@ -2,6 +2,7 @@ function gestionarFicheroXML(xmlDoc){
 alert (xmlDoc)
 	let capaVacia = document.querySelector("#ficheroXML")
 	let libros = xmlDoc.querySelectorAll("libro")
+	libro[2].className="fondo"
 	for(let i=0; i<libros.length; i++)
 		capaVacia.innerHTML = capaVacia.innerHTML + "<p>" + libros[i].textContent + "</p>"
 	
