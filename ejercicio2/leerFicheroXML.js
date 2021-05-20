c45e490834810457c04700a8166db55eec4e83d8
Marcos Tejera <76426487+XYUS66@users.noreply.github.com>
2021-05-20 20:48:40 +0200

    Add files via upload

--- ejercicio2/leerFicheroXML.js
+++ ejercicio2/leerFicheroXML.js
@@ -2,16 +2,11 @@
 function gestionarFicheroXML(xmlDoc){
 alert (xmlDoc)
 	let capaVacia = document.querySelector("#ficheroXML")
-	let izda = xmlDoc.querySelectorAll("izquierda")
   let dcha = xmlDoc.querySelectorAll("derecha")
-	for(let i=0; i<libros.length; i++)
-    if (i==2)
-    {
-        capaVacia.innerHTML = capaVacia.innerHTML + "<p class='fondo'>" + libros[i].textContent + "</p>"
-    }
-    else
-    {
+	let izda = xmlDoc.querySelectorAll("izquierda")
+  let fondo = xmlDoc.querySelectorAll("chat")
+  fondo.className="fondo"
+	for(let i=0; i<dcha.length; i++)
 		capaVacia.innerHTML = capaVacia.innerHTML + "<p>" + libros[i].textContent + "</p>"
-    }
 }
 loadDocA("chat.xml","xml")
